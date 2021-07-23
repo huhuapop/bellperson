d2ff8d516699c7b32dfd754716469780b1e131c7
huhuafupop <huhuapop@outlook.com>
2021-07-22 22:50:04 -0400

    check locked_kernel begin

--- src/gpu/locks.rs
+++ src/gpu/locks.rs
@@ -84,6 +84,7 @@ use crate::domain::create_fft_kernel;
 use crate::multiexp::create_multiexp_kernel;
 
 macro_rules! locked_kernel {
+    info!("locked_kernel begin");
     ($class:ident, $kern:ident, $func:ident, $name:expr) => {
         #[allow(clippy::upper_case_acronyms)]
         pub struct $class<E>
